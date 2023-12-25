756c71b92aa9516faf1e57144ea162eba0bb0473
devkrishna <ambatikrishnasai1301@gmail.com>
2023-12-25 16:42:51 +0530

    aoi changes

--- server/index.js
+++ server/index.js
@@ -215,7 +215,7 @@ app.get("/portfolio/:id", async (req, res) => {
 });
 
 //server
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 5000");
   connectDB();
 });
