bcb920f489a98c91005353ddd5e41982b8063b95
devkrishna <ambatikrishnasai1301@gmail.com>
2023-12-26 15:44:53 +0530

    api chnages

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+require("dotenv/config");
+
+const errorHandler = require("./helpers/error-handler.js/index.js");
 
 app.use(cors());
 app.options("*", cors());
@@ -13,6 +16,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("tiny"));
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use(errorHandler);
 
 const api = "/api/v1";
 const usersRoute = require("./routes/users");
@@ -21,6 +25,7 @@ const portfolioRoute = require("./routes/portfolios");
 const testRoute = require("./routes/tests");
 
 // Routes
+
 app.use(`${api}/users`, usersRoute);
 app.use(`${api}/deleteusers`, deleteUserRoute);
 app.use(`${api}/portfolios`, portfolioRoute);
