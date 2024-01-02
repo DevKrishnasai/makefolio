2876422ad4c0186229f7746edc4eba79fff63e68
devkrishna <ambatikrishnasai1301@gmail.com>
2024-01-02 14:52:06 +0530

    api changes

--- server/config/database.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: "mongodb+srv://devkrishna:7vK78RPLV7TexCz2@makfolio.r3cq38v.mongodb.net/portfolioDB",
-};
--- server/index.js
+++ server/index.js
@@ -6,36 +6,28 @@ const cors = require("cors");
 const app = express();
 require("dotenv/config");
 
-const errorHandler = require("./helpers/error-handler.js");
-
+// Middlewares
 app.use(cors());
 app.options("*", cors());
-
-// Middlewares
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("tiny"));
-app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
-app.use(errorHandler);
 
-const api = "/api/v1";
 const usersRoute = require("./routes/users.js");
 const deleteUserRoute = require("./routes/deleteusers.js");
 const portfolioRoute = require("./routes/portfolios.js");
 const testRoute = require("./routes/tests.js");
 
 // Routes
-
+const api = process.env.API;
 app.use(`${api}/users`, usersRoute);
 app.use(`${api}/deleteusers`, deleteUserRoute);
 app.use(`${api}/portfolios`, portfolioRoute);
 app.use(`${api}/tests`, testRoute);
 
-const dbConfig = require("./config/database.config.js");
-
 // Connecting to the database
 mongoose
-  .connect(dbConfig.url)
+  .connect(process.env.MONGO_DB_URL)
   .then(() => {
     console.log("Successfully connected to the database");
   })
@@ -49,6 +41,6 @@ app.get("/", (req, res) => {
 });
 
 // listen for requests
-app.listen(4006, () => {
-  console.log("Server is listening on port 4006");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is listening on port ${process.env.PORT || 5000}`);
 });
